9557f611ce08b5ac01f0c851dc181e2dd56a6732
dongyun <ehddbs452100@naver.com>
2024-04-21 18:43:26 +0900

    feat: Application

---
+++ src/main/java/midas/SoundOfFlower/SoundOfFlowerApplication.java
@@ -0,0 +1,13 @@
+package midas.SoundOfFlower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundOfFlowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoundOfFlowerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/midas/SoundOfFlower/SoundOfFlowerApplicationTests.java
@@ -0,0 +1,13 @@
+package midas.SoundOfFlower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundOfFlowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
