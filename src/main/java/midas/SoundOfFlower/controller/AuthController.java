3ca1bf70f34201ceaba9c75efbe24293866f43a8
dongyun <ehddbs452100@naver.com>
2024-04-21 18:34:32 +0900

    feat: Auth Controller

---
+++ src/main/java/midas/SoundOfFlower/controller/AuthController.java
@@ -0,0 +1,101 @@
+package midas.SoundOfFlower.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import midas.SoundOfFlower.dto.request.*;
+import midas.SoundOfFlower.jwt.dto.response.TokenResponse;
+import midas.SoundOfFlower.jwt.service.JwtService;
+import midas.SoundOfFlower.service.AuthService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Slf4j
+@RequestMapping("/auth")
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+    private final JwtService jwtService;
+
+    @PostMapping("/auth/email")
+    public ResponseEntity<Object> sendEmail(@RequestBody EmailRequest emailRequest) {
+        authService.sendEmail(emailRequest);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+    }
+
+    @PostMapping("/auth/register/authentication/number")
+    public ResponseEntity<Object> verifyEmail(@RequestBody VerifyEmailRequest verifyEmailRequest) {
+
+        authService.verifyEmail(verifyEmailRequest);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PostMapping("/resend-email")
+    public ResponseEntity<Object> reSendEmail(@RequestBody EmailRequest emailRequest) {
+
+        authService.sendEmail(emailRequest);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PostMapping("/auth/register/authentication/nickname")
+    public ResponseEntity<Object> verifyNickName(@RequestBody NickNameRequest nickNameRequest) {
+
+        authService.verifyNickName(nickNameRequest.getNickName());
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PostMapping("/auth/register")
+    public ResponseEntity<Object> signup(@RequestPart(value = "file", required = false) MultipartFile multipartFile, @Valid @RequestPart(value = "userRequestDto") UserRequest userRequest) throws IOException {
+
+        authService.signup(userRequest.getEmail(), userRequest.getPassword(), multipartFile, userRequest.getNickName());
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PostMapping("/auth/reset/password")
+    public ResponseEntity<Object> resetPassword(@Valid @RequestBody ResetPasswordRequest resetPasswordRequest) {
+
+        authService.resetPassword(resetPasswordRequest);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PostMapping("/token/logout")
+    public ResponseEntity<Object> logout(HttpServletRequest request) {
+
+        UserDetails principal = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String socialId = principal.getUsername();
+        log.info("socialId:{}", socialId);
+        String accessToken = jwtService.extractAccessToken(request).get();
+        authService.logout(accessToken, socialId);
+        SecurityContextHolder.clearContext();
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PostMapping("/token/reissue")
+    public ResponseEntity<Object> refresh(HttpServletRequest request, HttpServletResponse response) {
+
+        UserDetails principal = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String socialId = principal.getUsername();
+        String accessToken = jwtService.extractAccessToken(request).get();
+        String refreshToken = jwtService.extractRefreshToken(request).get();
+        TokenResponse tokenResponse = authService.validateToken(accessToken,refreshToken, socialId);
+        jwtService.setTokens(response, tokenResponse.getAccessToken(), tokenResponse.getRefreshToken());
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
