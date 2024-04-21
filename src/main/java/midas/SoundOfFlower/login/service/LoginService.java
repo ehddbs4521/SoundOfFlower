b521468d5d786fa14ec60f0eeb3c6308be67b860
dongyun <ehddbs452100@naver.com>
2024-04-21 18:39:47 +0900

    feat: Login Service

---
+++ src/main/java/midas/SoundOfFlower/login/service/LoginService.java
@@ -0,0 +1,33 @@
+package midas.SoundOfFlower.login.service;
+
+
+import lombok.RequiredArgsConstructor;
+import midas.SoundOfFlower.entity.User;
+import midas.SoundOfFlower.error.CustomException;
+import midas.SoundOfFlower.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import static midas.SoundOfFlower.error.ErrorCode.NOT_EXIST_USER_SOCIALID;
+
+
+@Service
+@RequiredArgsConstructor
+public class LoginService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String socialId) throws UsernameNotFoundException {
+        User user = userRepository.findBySocialId(socialId)
+                .orElseThrow(() -> new CustomException(NOT_EXIST_USER_SOCIALID));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getSocialId())
+                .password(user.getPassword())
+                .roles(user.getRole())
+                .build();
+    }
+}
