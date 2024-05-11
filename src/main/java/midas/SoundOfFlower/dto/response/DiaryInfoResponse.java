9f9c593e1a8aa6c4a156bdf07ceef437b190ad7c
dongyun <ehddbs452100@naver.com>
2024-05-11 18:19:22 +0900

    feat: Diary Info Response Dto

---
+++ src/main/java/midas/SoundOfFlower/dto/response/DiaryInfoResponse.java
@@ -0,0 +1,28 @@
+package midas.SoundOfFlower.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DiaryInfoResponse {
+
+    private String comment;
+    private String flower;
+
+    private Double angry;
+    private Double sad;
+    private Double delight;
+    private Double calm;
+    private Double embarrased;
+    private Double anxiety;
+
+    private Long musicId;
+    private String title;
+    private String singer;
+    private Double likes;
+}
