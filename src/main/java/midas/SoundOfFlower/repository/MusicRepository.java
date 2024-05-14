bdc651c34c6245c057d7ac5c9fb45f09015e8e3e
dongyun <ehddbs452100@naver.com>
2024-05-14 21:40:41 +0900

    feat: Music Repository

--- src/main/java/midas/SoundOfFlower/repository/MusicRepository.java
+++ src/main/java/midas/SoundOfFlower/repository/MusicRepository.java
@@ -4,7 +4,10 @@ import midas.SoundOfFlower.entity.Music;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MusicRepository extends JpaRepository<Music, Long> {
 
+    Optional<Music> findByMusicId(Long musicId);
 }
