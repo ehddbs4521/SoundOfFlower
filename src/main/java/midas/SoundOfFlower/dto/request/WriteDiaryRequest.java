c0137cbabcae7d349de97ef1a57b077703b56ecb
dongyun <ehddbs452100@naver.com>
2024-05-14 21:40:53 +0900

    feat: Write Content Request DTO

---
+++ src/main/java/midas/SoundOfFlower/dto/request/WriteDiaryRequest.java
@@ -0,0 +1,15 @@
+package midas.SoundOfFlower.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class WriteDiaryRequest {
+
+    private String content;
+}
