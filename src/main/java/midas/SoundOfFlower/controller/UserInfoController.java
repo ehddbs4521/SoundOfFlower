77b244d791f06eaa23f7f82aed49b68e311423fd
dongyun <ehddbs452100@naver.com>
2024-05-20 20:05:10 +0900

    feat: Get Statistical Data Of Emotion

--- src/main/java/midas/SoundOfFlower/controller/UserInfoController.java
+++ src/main/java/midas/SoundOfFlower/controller/UserInfoController.java
@@ -2,10 +2,12 @@ package midas.SoundOfFlower.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import midas.SoundOfFlower.dto.request.DateRequest;
 import midas.SoundOfFlower.dto.request.NickNameRequest;
 import midas.SoundOfFlower.dto.request.ValidateNickNameRequest;
-import midas.SoundOfFlower.dto.response.ModifyAttributeResponse;
+import midas.SoundOfFlower.dto.response.StatisticalEmotionResponse;
 import midas.SoundOfFlower.service.AuthService;
+import midas.SoundOfFlower.service.DiaryService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -15,6 +17,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Slf4j
@@ -23,6 +26,7 @@ import java.util.Map;
 public class UserInfoController {
 
     private final AuthService authService;
+    private final DiaryService diaryService;
 
     @PutMapping("/reset/nickname")
     public ResponseEntity<Object> changeNickName(@RequestBody ValidateNickNameRequest validateNickNameRequest) {
@@ -33,13 +37,24 @@ public class UserInfoController {
     }
 
     @PutMapping("/reset/profile")
-    public ResponseEntity<Object> changeProfile(@RequestPart(value = "nickName") NickNameRequest nickNameRequest, @RequestPart(value = "file") MultipartFile multipartFile) throws IOException {
+    public ResponseEntity<Object> changeProfile(@RequestPart(value = "nickName") NickNameRequest nickNameRequest, @RequestPart(value = "images") MultipartFile multipartFile) throws IOException {
 
         String updateProfileUrl = authService.updateProfileUrl(multipartFile, nickNameRequest.getNickName());
         Map<String, String> profile = new HashMap<>();
         profile.put("url", updateProfileUrl);
 
         return ResponseEntity.ok(profile);
+    }
+
+    @GetMapping("/statistic/emotion")
+    public ResponseEntity<Object> getStatisticalEmotion(@RequestBody DateRequest dateRequest) {
+
+        UserDetails principal = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String socialId = principal.getUsername();
+
+        List<StatisticalEmotionResponse> statisticalEmotion = diaryService.getStatisticalEmotion(dateRequest, socialId);
+
+        return ResponseEntity.ok(statisticalEmotion);
     }
 
 }
